bd5d3ad8b115744c284148380c8fa45ec776909d
= <=>
2015-07-25 22:41:24 -0700

    this is a commit

--- client/app/main/main.js
+++ client/app/main/main.js
@@ -8,4 +8,16 @@ angular.module('searsdropApp')
         templateUrl: 'app/main/main.html',
         controller: 'MainCtrl'
       });
-  });
+  }).directive("include", function ($http, $templateCache, $compile) {
+    return {
+        restrict: 'A',
+        link: function (scope, element, attributes) {
+            var templateUrl = scope.$eval(attributes.include);
+            $http.get(templateUrl, {cache: $templateCache}).success(
+                function (tplContent) {
+                    element.replaceWith($compile(tplContent)(scope));
+                }
+            );
+        }
+    };
+});
