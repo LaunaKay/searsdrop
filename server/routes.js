718f338dfb6781043f223319ac1c3822d9108d82
= <=>
2015-07-25 23:36:40 -0700

    fixed routes.js

--- server/routes.js
+++ server/routes.js
@@ -12,9 +12,13 @@ module.exports = function(app) {
   // Insert routes below
   app.use('/api/things', require('./api/thing'));
   app.use('/api/users', require('./api/user'));
+  app.use('/api/products', require('./api/product'));
+  app.use('/api/sales', require('./api/sale'));
+  app.use('/api/snapshots', require('./api/snapshot'));
+  app.use('/api/wishlists', require('./api/wishlist'));
 
   app.use('/auth', require('./auth'));
-  
+
   // All undefined asset or api routes should return a 404
   app.route('/:url(api|auth|components|app|bower_components|assets)/*')
    .get(errors[404]);
